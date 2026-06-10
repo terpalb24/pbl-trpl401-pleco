0209466cfc0e404f561f668e685515d8737a6ed7
Alldreno Hosea <Alldreno.4342401059@students.polibatam.ac.id>
2026-06-10 23:55:48 +0700

    Remove unused column and add column for robot's location

--- webapp/database/migrations/2026_04_27_053106_create_robots_table.php
+++ webapp/database/migrations/2026_04_27_053106_create_robots_table.php
@@ -11,15 +11,8 @@ return new class extends Migration
         Schema::create('robots', function (Blueprint $table) {
             $table->uuid('robot_id')->primary();
             $table->string('robot_name', 32);
-            $table->uuid('owner_id');
-            $table->string('api_key', 64)->unique()->nullable();
-
-            $table->foreign('owner_id')
-                ->references('account_id')
-                ->on('accounts')
-                ->onDelete('cascade');
-
-            $table->index(['owner_id']);
+            $table->string('api_key', 64)->unique();
+            $table->string('location_coordinates', 64)->unique()->nullable();
         });
     }
 
